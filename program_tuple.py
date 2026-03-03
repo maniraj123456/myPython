59ed04d50c3aa2458f66b559cbda46dc91a09b28
maniraj lingala <lingalamaniraj123@gmail.com>
2026-03-03 21:46:26 +0530

    tuple example

---
+++ program_tuple.py
@@ -0,0 +1,14 @@
+"""This program is to demonstrate the use of list in python"""
+# name = []
+
+# for i in range(0,7):
+#     name.append(input("enter name: "))
+# print(name)
+
+
+marks = []
+for i in range(0, 7):
+    marks.append(int(input("enter marks: ")))
+marks.sort()
+print(marks)
+# hello world

60c51a7fc3a5ce83d6ce1c0930cf46ea504348bd
maniraj lingala <lingalamaniraj123@gmail.com>
2026-03-03 10:21:47 +0530

    changes for lint

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-# sample programs are written to remeber the python syntax
+"""
+This module contains sample programs to help remember Python syntax.
+"""
 
 print('Learning python from scratch in 2026')
 

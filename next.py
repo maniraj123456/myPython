3a19368b75f18e561012f003309eabe64eeb25ef
maniraj lingala <lingalamaniraj123@gmail.com>
2026-03-03 12:59:46 +0530

    sample testing

--- next.py
+++ next.py
@@ -1,6 +1,10 @@
+"""This is a simple Python script that demonstrates basic list manipulation."""
+
 numbers = []
-for i in range(0,10):
+for i in range(0, 10):
     numbers.append(i)
 
 print(numbers)
+
+print("Hello, World!")
 # End-of-file (EOF)

1e4dd65da18efb5840f38e89eba412aadb495f32
maniraj lingala <lingalamaniraj123@gmail.com>
2026-03-04 09:12:54 +0530

    dictionary example

---
+++ program_dictonary.py
@@ -0,0 +1,15 @@
+"""This module contains a dictionary of programs and their corresponding functions."""
+
+emptyDict = {}  # creating an empty dictionary
+
+dict1 = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York",
+}  # creating a dictionary with some key-value pairs
+print(dict1.get("name"))  # accessing the value associated with the key "name"
+print(dict1["name"])  # accessing the value associated with the key "name"
+
+print(dict1.get("country"))  # accessing the value associated with the key "country"
+#print(dict1["country"])   accessing the value associated with the key "country"
+#The above line will raise a KeyError because the key 'country' does not exist in the dictionary.
